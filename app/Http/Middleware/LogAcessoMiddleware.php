b2171f400431caed06fb86ef37ee4b437945e6b4
Dan <dveras1623@gmail.com>
2022-07-02 16:48:04 -0300

    :pencil2: Salvando log no BD

--- app/Http/Middleware/LogAcessoMiddleware.php
+++ app/Http/Middleware/LogAcessoMiddleware.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use App\LogAcesso;
 
 class LogAcessoMiddleware
 {
@@ -15,6 +16,12 @@ class LogAcessoMiddleware
      */
     public function handle($request, Closure $next)
     {
-        return $next($request);
+
+        $ip = $request->server->get('REMOTE_ADDR');
+        $rota = $request->getRequestUri();
+        LogAcesso::create(['log' =>"IP $ip requisitou a rota $rota"]);
+        
+        //return $next($request);
+        return Response('Paramos no middleware');
     }
 }
