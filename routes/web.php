9bd956d31cd9dce125bc02f66a04f79b351f417e
Dan <dveras1623@gmail.com>
2022-07-02 15:14:11 -0300

    :pencil2: add middleware nas rotas

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Middleware\LogAcessoMiddleware;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,7 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'PrincipalController@principal' )->name('site.index');
+Route::middleware(LogAcessoMiddleware::class)
+    ->get('/', 'PrincipalController@principal')
+    ->name('site.index');
 Route::get('/sobre-nos', 'SobreNosController@sobreNos' )->name('site.sobrenos');
 Route::get('/contato', 'ContatoController@contato' )->name('site.contato');
 Route::post('/contato', 'ContatoController@salvar' )->name('site.contato');
