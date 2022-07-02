63c78460fb1d7af85b66cdaf50e653f573ca6896
Dan <dveras1623@gmail.com>
2022-07-02 17:18:23 -0300

    :pencil: Add middleware para todas a rotas

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Middleware\LogAcessoMiddleware;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,13 +13,11 @@ use App\Http\Middleware\LogAcessoMiddleware;
 |
 */
 
-Route::middleware(LogAcessoMiddleware::class)
-    ->get('/', 'PrincipalController@principal')
-    ->name('site.index');
+Route::get('/', 'PrincipalController@principal')->name('site.index');
+
 Route::get('/sobre-nos', 'SobreNosController@sobreNos' )->name('site.sobrenos');
 Route::get('/contato', 'ContatoController@contato' )->name('site.contato');
 Route::post('/contato', 'ContatoController@salvar' )->name('site.contato');
-
 Route::get('/login', function(){ return 'Login'; })->name('site.login');
 
 Route::prefix('/app')->group(function() {
