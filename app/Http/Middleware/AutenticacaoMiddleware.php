a3ecc999d4f1e52bff89ec62b8b8a5546671241b
Dan <dveras1623@gmail.com>
2022-07-03 20:43:16 -0300

    :wrench: Ajuste no middleware

--- app/Http/Middleware/AutenticacaoMiddleware.php
+++ app/Http/Middleware/AutenticacaoMiddleware.php
@@ -8,18 +8,13 @@ class AutenticacaoMiddleware
 {
     public function handle($request, Closure $next, $metodo_autenticacao, $perfil)
     {
-        // Verificar se o usuario possui acesso a rota;
-        echo $metodo_autenticacao.' - '.$perfil.'<br>';
+        session_start();
 
-        if($metodo_autenticacao == 'padrao') {
-            echo 'Verificar o usuario e senha no BD'.' - '.$perfil.'<br>';
+        if(isset($_SESSION['email']) && $_SESSION['email'] != '') {
+            return $next($request);
+        } else {
+            return redirect()->route('site.login', ['erro' => 2]);
         }
 
-        if($metodo_autenticacao == 'ldap') {
-            echo 'Verificar o usuario e senha no AD'.' - '.$perfil.'<br>';
-        }
-
-        //return $next($request);
-        return Response('Acesso negado! A rota acessada precisa de autenticação!!!');
     }
 }
