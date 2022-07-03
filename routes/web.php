93ad0ec7d1fffb72a013cb6c8bbb9ef662b84dc8
Dan <dveras1623@gmail.com>
2022-07-02 21:36:49 -0300

    :wrench: Ajustando rotas

--- routes/web.php
+++ routes/web.php
@@ -14,13 +14,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', 'PrincipalController@principal')->name('site.index');
-
 Route::get('/sobre-nos', 'SobreNosController@sobreNos' )->name('site.sobrenos');
 Route::get('/contato', 'ContatoController@contato' )->name('site.contato');
 Route::post('/contato', 'ContatoController@salvar' )->name('site.contato');
-Route::get('/login', function(){ return 'Login'; })->name('site.login');
 
-Route::prefix('/app')->group(function() {
+Route::get('/login', 'LoginController@index')->name('site.login');
+Route::post('/login', 'LoginController@autenticar')->name('site.login');
+
+Route::middleware('autenticacao:padrao,visitante')->prefix('/app')->group(function() {
     Route::get('/clientes', function(){ return 'Clientes'; })->name('app.clientes');
     Route::get('/fornecedores', 'FornecedorController@index' )->name('app.fornecedores');
     Route::get('/produtos', function(){ return 'Produtos'; })->name('app.produtos');
