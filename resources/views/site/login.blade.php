51e13a7df2dab78eb397387847baad0600607e07
Dan <dveras1623@gmail.com>
2022-07-03 20:44:04 -0300

    :sparkles: Create view login

---
+++ resources/views/site/login.blade.php
@@ -0,0 +1,49 @@
+@extends('site.layouts.index')
+
+@section('titulo', $titulo)
+
+@section('conteudo')
+    <div class="conteudo-pagina">
+        <div class="titulo-pagina">
+            <h1>Login</h1>
+        </div>
+
+        <div class="informacao-pagina">
+            <div style="width:30%; margin-left:auto; margin-right:auto;">
+
+                {{-- Mensagem de erro caso dados estejam errados --}}
+                {{ isset($erro) && $erro != '' ? $erro : ''  }}
+
+                <form action={{ route('site.login') }} method="post">
+                    @csrf
+                    <input name="usuario" value="{{ old('usuario') }}" type="text" placeholder="Usuário" class="borda-preta">
+                        {{ $errors->has('usuario') ? $errors->first('usuario') : '' }}
+                    <input name="senha" value="{{ old('senha') }}" type="password" placeholder="Senha" class="borda-preta">
+                        {{ $errors->has('senha') ? $errors->first('senha') : '' }}
+                    <button type="submit" class="borda-preta">Acessar</button>    
+                </form>
+            </div>
+        </div>
+    </div>
+
+    <div class="rodape">
+        <div class="redes-sociais">
+            <h2>Redes sociais</h2>
+            <img src="{{ asset('img/facebook.png') }}">
+            <img src="{{ asset('img/linkedin.png') }}">
+            <img src="{{ asset('img/youtube.png') }}">
+        </div>
+
+        <div class="area-contato">
+            <h2>Contato</h2>
+            <span>(86) 4321-1234</span>
+            <br>
+            <span>supergestao@dominio.com.br</span>
+        </div>
+        
+        <div class="localizacao">
+            <h2>Localização</h2>
+            <img src="{{ asset('img/mapa.png') }}">
+        </div>
+    </div>
+@endsection
