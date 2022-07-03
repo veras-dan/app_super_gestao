a780db71c73f512892f192abb09909b8ebf0a030
Dan <dveras1623@gmail.com>
2022-07-03 20:41:03 -0300

    :pencil: ajuste nas rotas

--- routes/web.php
+++ routes/web.php
@@ -18,13 +18,16 @@ Route::get('/sobre-nos', 'SobreNosController@sobreNos' )->name('site.sobrenos');
 Route::get('/contato', 'ContatoController@contato' )->name('site.contato');
 Route::post('/contato', 'ContatoController@salvar' )->name('site.contato');
 
-Route::get('/login', 'LoginController@index')->name('site.login');
+Route::get('/login/{erro?}', 'LoginController@index')->name('site.login');
 Route::post('/login', 'LoginController@autenticar')->name('site.login');
 
 Route::middleware('autenticacao:padrao,visitante')->prefix('/app')->group(function() {
-    Route::get('/clientes', function(){ return 'Clientes'; })->name('app.clientes');
-    Route::get('/fornecedores', 'FornecedorController@index' )->name('app.fornecedores');
-    Route::get('/produtos', function(){ return 'Produtos'; })->name('app.produtos');
+    Route::get('/home', 'HomeController@index' )->name('app.home');
+    Route::get('/sair', 'LoginController@sair' )->name('app.sair');
+
+    Route::get('/cliente', 'ClienteController@index')->name('app.cliente');
+    Route::get('/fornecedor', 'FornecedorController@index' )->name('app.fornecedor');
+    Route::get('/produto', 'ProdutoController@index' )->name('app.produto');
 });
 
 Route::fallback(function() {
