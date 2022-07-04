5991e7926a773429f7b26ba33de19aa31e3fff7b
Dan <dveras1623@gmail.com>
2022-07-04 00:12:50 -0300

    :pencil: renomeando rota

--- app/Http/Controllers/FornecedorController.php
+++ app/Http/Controllers/FornecedorController.php
@@ -2,11 +2,51 @@
 
 namespace App\Http\Controllers;
 
+use App\Fornecedor;
 use Illuminate\Http\Request;
 
 class FornecedorController extends Controller
 {
     public function index() {
-        return view('app.fornecedor');
+        return view('app.fornecedor.index');
+    }
+
+    public function listar() {
+        return view('app.fornecedor.listar');
+    }
+
+    public function adicionar(Request $request) {
+        
+        $msg = '';
+
+        if($request->input('_token') != '') {
+            // Validação
+            $regras = [
+                'nome' => 'required|min:3|max:40',
+                'site' => 'required',
+                'telefone' => 'required',
+                'uf' => 'required|min:2|max:2',
+                'email' => 'email'
+            ];
+
+            $feedback = [
+                'required' => 'O campo :attribute deve ser preenchido',
+                'nome.min' => 'O campo nome deve ter no mínimo 3 caracteres',
+                'nome.max' => 'O campo nome deve ter no máximo 40 caracteres',
+                'uf.min' => 'O campo uf deve ter no mínimo 2 caracteres',
+                'uf.max' => 'O campo uf deve ter no máximo 2 caracteres',
+                'email.email' => 'O campo email não foi preenchido corretamente'
+            ];
+
+            $request->validate($regras, $feedback);
+
+            $fornecedor = new Fornecedor();
+            $fornecedor::create($request->all());
+
+            // Mensagem de cadastro realizado com sucesso;
+            $msg = 'Cadastro realizado com sucesso!';
+        }
+
+        return view('app.fornecedor.adicionar', ['msg' => $msg]);
     }
 }
