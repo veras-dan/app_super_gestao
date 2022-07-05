1e141390979aaafb2720cd82e9e66b1c1b6d89e5
Dan <dveras1623@gmail.com>
2022-07-05 20:55:28 -0300

    :sparkles: Create new view

---
+++ resources/views/app/produto/edit.blade.php
@@ -0,0 +1,47 @@
+@extends('app.layouts.index')
+
+@section('titulo', 'Produto')
+    
+@section('conteudo')
+    <div class="conteudo-pagina">
+        <div class="titulo-pagina-2">
+            <p>Editar Produto</p>
+        </div>
+
+        <div class="menu">
+            <ul>
+                <li><a href="{{ route('produto.index') }}">Voltar</a></li>
+                <li><a href="">Consulta</a></li>
+            </ul>
+        </div>
+
+        <div class="informacao-pagina">
+            <div style="width:30%; margin-left:auto; margin-right:auto;">
+                <form action="{{ route('produto.update', ['produto' => $produto->id]) }}" method="post">
+                    @csrf
+                    @method('PUT')
+                    <input type="text" value="{{ $produto->nome ?? old('nome') }}" name="nome" placeholder="Nome" class="borda-preta">
+                        {{ $errors->has('nome') ? $errors->first('nome') : '' }}
+
+                    <input type="text" value="{{ $produto->descricao ?? old('descricao') }}" name="descricao" placeholder="Descrição" class="borda-preta">
+                        {{ $errors->has('descricao') ? $errors->first('descricao') : '' }}
+
+                    <input type="text" value="{{ $produto->peso ?? old('peso') }}" name="peso"  placeholder="Peso" class="borda-preta">
+                        {{ $errors->has('peso') ? $errors->first('peso') : '' }}
+
+                    <select name="unidade_id">
+                        <option value="">-- Selecione uma unidade --</option>
+                        
+                        @foreach ($unidades as $unidade)
+                            <option value="{{ $unidade->id }}" {{ ($produto->unidade_id ?? old('unidade_id')) == $unidade->id ? 'selected' : '' }}>{{ $unidade->descricao }}</option>
+                        @endforeach
+
+                    </select>
+                        {{ $errors->has('unidade_id') ? $errors->first('unidade_id') : '' }}
+
+                    <button type="subimit" class="borda-preta">Cadastrar</button>
+                </form>
+            </div>
+        </div>
+    </div>
+@endsection
