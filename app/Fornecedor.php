925d34fd57743854ed2e6b9e95489895ce1f309d
Dan <dveras1623@gmail.com>
2022-07-07 12:57:12 -0300

    :sparkles: Add hasMany

--- app/Fornecedor.php
+++ app/Fornecedor.php
@@ -3,10 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Fornecedor extends Model
 {
+    // SoftDeletes
     use SoftDeletes;
 
     // Seleciona a tabela desejada
@@ -15,4 +17,8 @@ class Fornecedor extends Model
     protected $fillable = [
         'nome', 'site', 'telefone', 'uf', 'email'
     ];
+
+    public function produtos() {
+        return $this->HasMany('App\Item', 'fornecedor_id', 'id');    
+    }
 }
