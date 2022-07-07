85b267abfc8f26c1280741eec789a30985b48ca1
Dan <dveras1623@gmail.com>
2022-07-06 22:02:02 -0300

    :sparkles: Create new views

---
+++ resources/views/app/produto_detalhe/create.blade.php
@@ -0,0 +1,46 @@
+@extends('app.layouts.index')
+
+@section('titulo', 'Produto')
+    
+@section('conteudo')
+    <div class="conteudo-pagina">
+        <div class="titulo-pagina-2">
+            <p>Adicionar - Produto</p>
+        </div>
+
+        <div class="menu">
+            <ul>
+                <li><a href="{{ route('produto.index') }}">Voltar</a></li>
+                <li><a href="">Consulta</a></li>
+            </ul>
+        </div>
+
+        <div class="informacao-pagina">
+            <div style="width:30%; margin-left:auto; margin-right:auto;">
+                <form action="{{ route('produto.store') }}" method="post">
+                    @csrf
+                    <input type="text" value="{{ old('nome') }}" name="nome" placeholder="Nome" class="borda-preta">
+                        {{ $errors->has('nome') ? $errors->first('nome') : '' }}
+
+                    <input type="text" value="{{ old('descricao') }}" name="descricao" placeholder="Descrição" class="borda-preta">
+                        {{ $errors->has('descricao') ? $errors->first('descricao') : '' }}
+
+                    <input type="text" value="{{ old('peso') }}" name="peso"  placeholder="Peso" class="borda-preta">
+                        {{ $errors->has('peso') ? $errors->first('peso') : '' }}
+
+                    <select name="unidade_id">
+                        <option value="">-- Selecione uma unidade --</option>
+                        
+                        @foreach ($unidades as $unidade)
+                            <option value="{{ $unidade->id }}" {{ old('unidade_id') == $unidade->id ? 'selected' : '' }}>{{ $unidade->descricao }}</option>
+                        @endforeach
+
+                    </select>
+                        {{ $errors->has('unidade_id') ? $errors->first('unidade_id') : '' }}
+
+                    <button type="subimit" class="borda-preta">Cadastrar</button>
+                </form>
+            </div>
+        </div>
+    </div>
+@endsection
