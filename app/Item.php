24043bcf39e3e10046b03b94dffda36937fa0c81
Dan <dveras1623@gmail.com>
2022-07-07 12:57:43 -0300

    :sparkles: Add hasOne

--- app/Item.php
+++ app/Item.php
@@ -11,8 +11,10 @@ class Item extends Model
     protected $fillable = ['nome', 'descricao', 'peso', 'unidade_id'];
 
     public function itemDetalhe() {
-        return $this->hasOne('App\ItemDetalhe', 'produto_id', 'id');
+        return $this->hasOne('App\ItemDetalhe', 'produto_id', 'id'); 
+    }
 
-        
+    public function fornecedor() {
+        return $this->belongsTo('App\Fornecedor'); 
     }
 }
