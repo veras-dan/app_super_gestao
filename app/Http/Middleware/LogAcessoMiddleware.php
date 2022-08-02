9939f9ef1a5651fe68ea4659239b1d7091efacd9
Dan <dveras1623@gmail.com>
2022-08-02 10:08:13 -0300

    Update LogAcessoMiddleware.php

--- app/Http/Middleware/LogAcessoMiddleware.php
+++ app/Http/Middleware/LogAcessoMiddleware.php
@@ -1,7 +1,6 @@
 <?php
 
 namespace App\Http\Middleware;
-
 use Closure;
 use App\LogAcesso;
 
@@ -16,7 +15,6 @@ class LogAcessoMiddleware
      */
     public function handle($request, Closure $next)
     {
-
         $ip = $request->server->get('REMOTE_ADDR');
         $rota = $request->getRequestUri();
         LogAcesso::create(['log' =>"IP $ip requisitou a rota $rota"]);
