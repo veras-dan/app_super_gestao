91a4911a934568494550e96a96534abc95681edf
Dan <veras_dan@outlook.com>
2023-08-07 21:22:30 -0300

    :pencil: Corrigindo Rotas
    Organizando melhor os grupos e rotas

--- routes/web.php
+++ routes/web.php
@@ -3,14 +3,13 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'PrincipalController@principal');
-
 Route::get('/contato', 'ContatoController@contato');
-
 Route::get('/sobre-nos', 'SobreNosController@sobreNos');
-
-
-
 Route::get('/login', function(){ return 'Login'; });
-Route::get('/clientes', function(){ return 'Clientes'; });
-Route::get('/fornecedores', function(){ return 'Fornecedores'; });
-Route::get('/produtos', function(){ return 'Produtos'; });
+
+Route::prefix('/app')->group(function() {
+    Route::get('/clientes', function(){ return 'Clientes'; });
+    Route::get('/fornecedores', function(){ return 'Fornecedores'; });
+    Route::get('/produtos', function(){ return 'Produtos'; });
+
+});
