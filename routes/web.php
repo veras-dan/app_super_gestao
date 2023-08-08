aad0eb9f8de81f19ab9f8c547e7ad0a860cd8611
Dan <veras_dan@outlook.com>
2023-08-07 22:30:52 -0300

    :pencil2:  nomeando rotas

--- routes/web.php
+++ routes/web.php
@@ -2,14 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', 'PrincipalController@principal');
-Route::get('/contato', 'ContatoController@contato');
-Route::get('/sobre-nos', 'SobreNosController@sobreNos');
-Route::get('/login', function(){ return 'Login'; });
+Route::get('/', 'PrincipalController@principal')->name('site.index');
+Route::get('/contato', 'ContatoController@contato')->name('site.contato');
+Route::get('/sobre-nos', 'SobreNosController@sobreNos')->name('site.sobrenos');
+Route::get('/login', function(){ return 'Login'; })->name('site.login');
 
 Route::prefix('/app')->group(function() {
-    Route::get('/clientes', function(){ return 'Clientes'; });
-    Route::get('/fornecedores', function(){ return 'Fornecedores'; });
-    Route::get('/produtos', function(){ return 'Produtos'; });
-
+    Route::get('/clientes', function(){ return 'Clientes'; })->name('app.clientes');
+    Route::get('/fornecedores', function(){ return 'Fornecedores'; })->name('app.fornecedores');
+    Route::get('/produtos', function(){ return 'Produtos'; })->name('app.produtos');
 });
